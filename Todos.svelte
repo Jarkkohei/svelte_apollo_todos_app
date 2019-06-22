fa0db06ae1fc4861925a36f961da8ac3d0298e4e
Jarkkohei <32803046+Jarkkohei@users.noreply.github.com>
2019-06-22 23:42:28 +0300

    Fetch and display Todos

--- Todos.svelte
+++ Todos.svelte
@@ -1,1 +1,32 @@
+<script>
+  import { getClient, query } from "svelte-apollo";
+  import { gql } from "apollo-boost";
+
+  const GET_TODOS = gql`
+    {
+      getTodos {
+        id
+        text
+        done
+      }
+    }
+  `;
+
+  const client = getClient();
+
+  const todos = query(client, { query: GET_TODOS });
+</script>
+
 <h2>Todos</h2>
+{#await $todos}
+  Loading todos...
+{:then result}
+  <p>Total todos: {result.data.getTodos.length}</p>
+  {#each result.data.getTodos as todo, index}
+    <li>
+      {todo.text}
+    </li>
+  {/each}
+{:catch error}
+  <p>{error}</p>
+{/await}
